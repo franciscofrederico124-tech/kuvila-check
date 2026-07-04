b1da03007ef224d3826cf1c0077ec20f6c9d26e9
franciscofrederico124-tech <franciscofrederico124@gmail.com>
2026-07-04 18:55:19 +0100

    versiion with the message system  Version 6.0.0

--- gateway/target/debug/deps/gateway.d
+++
@@ -1,5 +0,0 @@
-C:\Users\PC\Desktop\kuvila\gateway\target\debug\deps\gateway.d: src\main.rs
-
-C:\Users\PC\Desktop\kuvila\gateway\target\debug\deps\gateway.exe: src\main.rs
-
-src\main.rs:
--- gateway/target/debug/gateway.d
+++
@@ -1,1 +0,0 @@
-C:\Users\PC\Desktop\kuvila\gateway\target\debug\gateway.exe: C:\Users\PC\Desktop\kuvila\gateway\src\main.rs
