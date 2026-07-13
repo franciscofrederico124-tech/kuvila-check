9cc4e5e5230b3704d58489c6f69bf44f9ba6a84c
franciscofrederico124-tech <franciscofrederico124@gmail.com>
2026-07-13 18:06:41 +0100

    Título da bomba corrigido

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: true,
+        secure: false,
         maxAge: 1000 * 60 * 60,
     }
 }));
