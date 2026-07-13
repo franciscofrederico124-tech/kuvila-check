d7eae7e4203d2d4c4c1cd9f5ec76ee620bd15db9
franciscofrederico124-tech <franciscofrederico124@gmail.com>
2026-07-13 18:43:00 +0100

    Version secure

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 60,
     }
 }));
